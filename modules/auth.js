2fbc9984261d6e199e7c490340e4ec5585fbfc9b
Mark Tyers <marktyers@gmail.com>
2015-12-14 07:22:21 +0000

    auth module working

--- modules/auth.js
+++ modules/auth.js
@@ -1,25 +1,43 @@
 
 "use strict"
 
+var bcrypt = require('bcrypt')
 var storage = require('node-persist')
 storage.initSync()
 
 // node-persist example in 2015 topic 7 shopping.
 
 exports.createAccount = (username, password) => {
-	return {status: 'success', username: 'testuser'}
+	if (username.length === 0) {
+		throw new Error('username parameter missing')
+	}
+	const salt = bcrypt.genSaltSync(10)
+	const hash = bcrypt.hashSync(password, salt)
+	if (storage.getItemSync(username) !== undefined) {
+		throw new Error('account '+username+' already exists')
+	}
+	storage.setItem(username, {username: username, hash: hash})
+	return {status: 'success', username: username}
 }
 
 exports.logIn = (username, password) => {
-	return true
+	if (storage.getItemSync(username) === undefined) {
+		throw new Error('account '+username+' does not exist exists')
+	}
+	const account = storage.getItemSync(username)
+	if (!bcrypt.compareSync(password, account.hash)) {
+		throw new Error('invalid password')
+	}
+	return {username: username}
 }
 
 exports.clearAccounts = () => {
+	storage.clearSync()
 	return true
 }
 
 exports.countAccounts = () => {
-	return 1
+	return storage.values().length
 }
 
 exports.count = 1
--- spec/auth-spec.js
+++ spec/auth-spec.js
@@ -22,4 +22,39 @@ describe('create accounts', () => {
 			expect(auth.count).toBe(1)
 		}
 	})
+	
+	it('create a user with a different username/password', () => {
+		try {
+			let user = auth.createAccount('testuser2', 'password')
+			expect(user).toEqual({status: 'success', username: 'testuser2'})
+		} catch(err) {
+			fail('error should not be thrown')
+		} finally {
+			expect(auth.count).toBe(1)
+		}
+	})
+	
+	it('should prevent duplicate usernames  from being inserted', () => {
+		try {
+			auth.createAccount('testuser', 'p455w0rd')
+			expect(auth.createAccount('testuser', 'p455w0rd')).toThrow()
+			fail('this line should not be run')
+		} catch(err) {
+			expect(err.message).toBe('account testuser already exists')
+		} finally {
+			expect(auth.count).toBe(1)
+		}
+	})
+	
+	it('should throw an error if missing username', () => {
+		try {
+			auth.createAccount('testuser', 'p455w0rd')
+			expect(auth.createAccount('', 'p455w0rd')).toThrow()
+		} catch(err) {
+			expect(err.message).toBe('username parameter missing')
+		} finally {
+			expect(auth.count).toBe(1)
+		}
+	})
+	
 })
