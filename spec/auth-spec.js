d768d5375d6dfabe96902dcbc6b04483ac69e4e9
Mark Tyers <marktyers@gmail.com>
2015-12-14 07:46:43 +0000

    nested tests

--- spec/auth-spec.js
+++ spec/auth-spec.js
@@ -6,55 +6,91 @@ var rewire = require('rewire')
 
 var auth = rewire("../modules/auth")
 
-describe('create accounts', () => {
+describe('Auth Model', () => {
 	
 	beforeEach( () => {
 		auth.clearAccounts()
 	})
 	
-	it('create a single valid account', () => {
-		try {
-			let user = auth.createAccount('testuser', 'p455w0rd')
-			expect(user).toEqual({status: 'success', username: 'testuser'})
-		} catch(err) {
-			fail('error should not be thrown')
-		} finally {
-			expect(auth.count).toBe(1)
-		}
+	describe('create accounts', () => {
+	
+		it('create a single valid account', () => {
+			try {
+				let user = auth.createAccount('testuser', 'p455w0rd')
+				expect(user).toEqual({status: 'success', username: 'testuser'})
+			} catch(err) {
+				fail('error should not be thrown')
+			} finally {
+				expect(auth.count).toBe(1)
+			}
+		})
+	
+		it('create a user with a different username/password', () => {
+			try {
+				let user = auth.createAccount('testuser2', 'password')
+				expect(user).toEqual({status: 'success', username: 'testuser2'})
+			} catch(err) {
+				fail('error should not be thrown')
+			} finally {
+				expect(auth.count).toBe(1)
+			}
+		})
+	
+		it('should prevent duplicate usernames  from being inserted', () => {
+			try {
+				auth.createAccount('testuser', 'p455w0rd')
+				expect(auth.createAccount('testuser', 'p455w0rd')).toThrow()
+				fail('this line should not be run')
+			} catch(err) {
+				expect(err.message).toBe('account testuser already exists')
+			} finally {
+				expect(auth.count).toBe(1)
+			}
+		})
+	
+		it('should throw an error if missing username', () => {
+			try {
+				auth.createAccount('testuser', 'p455w0rd')
+				expect(auth.createAccount('', 'p455w0rd')).toThrow()
+			} catch(err) {
+				expect(err.message).toBe('username parameter missing')
+			} finally {
+				expect(auth.count).toBe(1)
+			}
+		})
 	})
 	
-	it('create a user with a different username/password', () => {
-		try {
-			let user = auth.createAccount('testuser2', 'password')
-			expect(user).toEqual({status: 'success', username: 'testuser2'})
-		} catch(err) {
-			fail('error should not be thrown')
-		} finally {
-			expect(auth.count).toBe(1)
-		}
-	})
+	describe('Log In', () => {
+		it('should allow registered user to login', () => {
+			try {
+				auth.createAccount('testuser', 'p455w0rd')
+				const res = auth.logIn('testuser', 'p455w0rd')
+				expect(res).toBeDefined()
+				expect(res.username).toBe('testuser')
+			} catch(err) {
+				fail('error should not be thrown')
+			}
+		})
 	
-	it('should prevent duplicate usernames  from being inserted', () => {
-		try {
-			auth.createAccount('testuser', 'p455w0rd')
-			expect(auth.createAccount('testuser', 'p455w0rd')).toThrow()
-			fail('this line should not be run')
-		} catch(err) {
-			expect(err.message).toBe('account testuser already exists')
-		} finally {
-			expect(auth.count).toBe(1)
-		}
-	})
+		it('should prevent unregistered user from logging in', () => {
+			try {
+				auth.createAccount('testuser', 'p455w0rd')
+				auth.logIn('testuser2', 'p455w0rd')
+				fail('error should have been thrown')
+			} catch(err) {
+				expect(err.message).toBe('account testuser2 does not exist')
+			}
+		})
 	
-	it('should throw an error if missing username', () => {
-		try {
-			auth.createAccount('testuser', 'p455w0rd')
-			expect(auth.createAccount('', 'p455w0rd')).toThrow()
-		} catch(err) {
-			expect(err.message).toBe('username parameter missing')
-		} finally {
-			expect(auth.count).toBe(1)
-		}
+		it('should prevent invalid password being accepted', () => {
+			try {
+				auth.createAccount('testuser', 'p455w0rd')
+				auth.logIn('testuser', 'password')
+				fail('error should have been thrown')
+			} catch(err) {
+				expect(err.message).toBe('invalid password')
+			}
+		})	
 	})
-	
+
 })
--- spec/books-spec.js
+++ spec/books-spec.js
@@ -11,64 +11,67 @@ function setData(file) {
 	})
 }
 
-describe("Search for a book", () => {
+describe('Book Model', () => {
 	
-	it('search for a recognised topic', done => {
-		setData('javascript.json')
-		books.search('javascript', 'http://example.com', (err, data) => {
-			console.log(JSON.stringify(data, null, 2))
-			expect(err).toBeNull()
-			expect(data).toBeDefined()
-			expect(data.length).toBe(3)
-			expect(data[0].title).toBe('Eloquent JavaScript, 2nd Ed.')
-			expect(data[0].link).toContain('http://example.com/books/')
-			done()
+	describe('search for books', () => {
+		
+		it('search for a recognised topic', done => {
+			setData('javascript.json')
+			books.search('javascript', 'http://example.com', (err, data) => {
+				expect(err).toBeNull()
+				expect(data).toBeDefined()
+				expect(data.length).toBe(3)
+				expect(data[0].title).toBe('Eloquent JavaScript, 2nd Ed.')
+				expect(data[0].link).toContain('http://example.com/books/')
+				done()
+			})
 		})
-	})
 	
-	it('search for an unknown topic', done => {
-		setData('unknown.json')
-		books.search('dgfuhalgux', 'http://example.com', (err, data) => {
-			expect(err).toBeDefined()
-			expect(err.message).toEqual('No Books Found')
-			done()
+		it('search for an unknown topic', done => {
+			setData('unknown.json')
+			books.search('dgfuhalgux', 'http://example.com', (err, data) => {
+				expect(err).toBeDefined()
+				expect(err.message).toEqual('No Books Found')
+				done()
+			})
 		})
-	})
+		
+		it('search with a missing query', done => {
+			setData('missing.json')
+			books.search('', 'http://example.com', (err, data) => {
+				expect(err).toBeDefined()
+				expect(err.message).toEqual('Missing Query Parameter')
+				done()
+			})
+		})
+
+		it('search with a null query', done => {
+			setData('missing.json')
+			books.search(null, 'http://example.com', (err, data) => {
+				expect(err).toBeDefined()
+				expect(err.message).toEqual('Missing Query Parameter')
+				done()
+			})
+		})
 	
-	it('search with a missing query', done => {
-		setData('missing.json')
-		books.search('', 'http://example.com', (err, data) => {
-			expect(err).toBeDefined()
-			expect(err.message).toEqual('Missing Query Parameter')
-			done()
+		it('search with missing domain parameter', done => {
+			setData('javascript.json')
+			books.search('javascript', '', (err, data) => {
+				expect(err).toBeDefined()
+				expect(err.message).toEqual('Missing Host Parameter')
+				done()
+			})
 		})
+		
+		it('search with null domain parameter', done => {
+			setData('javascript.json')
+			books.search('javascript', '', (err, data) => {
+				expect(err).toBeDefined()
+				expect(err.message).toEqual('Missing Host Parameter')
+				done()
+			})
+		})
+		
 	})
 
-	it('search with a null query', done => {
-		setData('missing.json')
-		books.search(null, 'http://example.com', (err, data) => {
-			expect(err).toBeDefined()
-			expect(err.message).toEqual('Missing Query Parameter')
-			done()
-		})
-	})
-	
-	it('search with missing domain parameter', done => {
-		setData('javascript.json')
-		books.search('javascript', '', (err, data) => {
-			expect(err).toBeDefined()
-			expect(err.message).toEqual('Missing Host Parameter')
-			done()
-		})
-	})
-	
-	it('search with null domain parameter', done => {
-		setData('javascript.json')
-		books.search('javascript', '', (err, data) => {
-			expect(err).toBeDefined()
-			expect(err.message).toEqual('Missing Host Parameter')
-			done()
-		})
-	})
-	
 })
